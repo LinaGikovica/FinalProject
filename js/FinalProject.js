ab580fcf9608fb7a880abbcb77826970302c247e
LinaGikovica <LinaGikovica@users.noreply.github.com>
2018-03-01 20:10:21 +0100

    displays json in modal

--- js/FinalProject.js
+++ js/FinalProject.js
@@ -11,8 +11,6 @@ class SearchBar extends React.Component {
   handleDateChange(e) {
     // parse the YYYY-MM date from the Input
     // and convert it to real Date() object
-    
-    // ex. 2012-01
     // toDate() turns moment-internal time format to JS Date()
     const date = moment(e.target.value, "YYYY-MM").toDate();
     this.props.onSearchDateChange(date);
@@ -38,12 +36,20 @@ class SearchBar extends React.Component {
   }
 }
 
+ // making one search result with image, title, description using linkpreview
+
 class ResultItem extends React.Component {
 
   constructor(props) {
     super(props);
 
+    this.handleClick = this.handleClick.bind(this);
+
     this.state = { }
+  }
+
+  handleClick() {
+    this.props.onItemSelected(this.props);
   }
 
   componentDidMount() {
@@ -58,30 +64,37 @@ class ResultItem extends React.Component {
     if(this.state.image != null) {
       image = <a href={this.props.url} target="_blank"><img className="articleImage" src={this.state.image}/></a>
     }
+      //meet my goat ;) for when api key expired or loading, this puts image anyway
     else {
       image = <a href={this.props.url} target="_blank"><img className="articleImage" src="https://i.imgur.com/UFwbr6k.jpg"/></a>
     }
     return (
       <div className="Item">
-        <h2>{this.props.title}</h2>
+        <h2 onClick={ this.handleClick }>{this.props.title}</h2>
         {image}
         <p>{this.props.description}</p>
       </div>
     );
   }
 }
-
+    
+// table made of 20 results
+    
 class ResultTable extends React.Component {
   constructor(props) {
     super(props);
+ this.handleChildSelected = this.handleChildSelected.bind(this);
 
     this.state = {
-      loaded: false,
       articles: []
     };
   }
 
-  // poziva se kada se komponenta zakaci na DOM
+  handleChildSelected(childItem) {
+    this.props.onResultSelected(childItem);
+  }
+
+   
   componentDidMount() {
     const component = this;
     const date = this.props.date;
@@ -92,11 +105,12 @@ class ResultTable extends React.Component {
     jQuery.get(url, data => {
       component.setState({
         loaded: true,
-        articles: data.response.docs.slice(0, 20)
+        articles: data.response.docs.slice(0, 20) //getting exactly 20 results
       });
     });
   }
-
+    
+// rectangle for loading (check spinner.css for spec)
   render() {
       
     if(!this.state.loaded)
@@ -111,7 +125,10 @@ class ResultTable extends React.Component {
             key={article._id}
             url={article.web_url}
             title={article.headline.main}
-            description={article.abstract || article.snippet}/>
+            description={article.abstract || article.snippet}
+            json={article}
+            onItemSelected={this.handleChildSelected}/>    
+          
         );
     });
 
@@ -122,15 +139,52 @@ class ResultTable extends React.Component {
     var response = $.ajax()
   }
 }
+class SingleArticleItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: true
+    }
+  }
+
+  componentWillReceiveProps() {
+    this.setState({ show: true });
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  render() {
+    if (this.props.article != null && this.state.show)
+    {
+      return (
+        <div className="modal">
+          <div className="modal-content">
+            <span onClick={this.handleClose} className="close">&times;</span>
+            <pre>{ JSON.stringify(this.props.article.json, null, ' ') }</pre>
+          </div>
+        </div>
+        );
+    }
+    return null;    
+  }
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleResultSelected = this.handleResultSelected.bind(this);
+ 
 
     this.state = {
-      searchDate: new Date
+      searchDate: new Date,
+      selectedArticle: null 
     };
   }
 
@@ -139,7 +193,13 @@ class App extends React.Component {
       searchDate: date
     });
   }
-
+  
+     handleResultSelected(result) {
+    this.setState({
+      selectedArticle: result
+    })
+  }
+    
   render() {
     return (
       <div className="App">
@@ -151,7 +211,9 @@ class App extends React.Component {
         <div className="Result">
         <ResultTable
           key={this.state.searchDate}
-          date={this.state.searchDate}/>
+          date={this.state.searchDate}
+          onResultSelected={this.handleResultSelected}/>
+        <SingleArticleItem article={this.state.selectedArticle}/>
         </div>
       </div>
     );
